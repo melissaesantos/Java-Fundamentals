e9c81208be691c656757ccd89fa5132d72d83cb0
Melissa Santos <melissasantos2202@gmail.com>
2024-07-25 15:21:00 -0600

    created array file

---
+++ arrays.java
@@ -0,0 +1,54 @@
+public class ArrayOperations {
+    
+    public static void main(String[] args) {
+        // Example usage of methods
+    }
+    
+    public static int findMax(int[] array) {
+        // Implementation
+    }
+
+    public static int findMin(int[] array) {
+        // Implementation
+    }
+
+    public static double calculateAverage(int[] array) {
+        // Implementation
+    }
+
+    public static void reverseArray(int[] array) {
+        // Implementation
+    }
+
+    public static void sortArrayAscending(int[] array) {
+        // Implementation
+    }
+
+    public static void sortArrayDescending(int[] array) {
+        // Implementation
+    }
+
+    public static int[] mergeArrays(int[] array1, int[] array2) {
+        // Implementation
+    }
+
+    public static int[] removeDuplicates(int[] array) {
+        // Implementation
+    }
+
+    public static void rotateArrayLeft(int[] array, int positions) {
+        // Implementation
+    }
+
+    public static void rotateArrayRight(int[] array, int positions) {
+        // Implementation
+    }
+
+    public static void print2DArray(int[][] array) {
+        // Implementation
+    }
+
+    public static int[][] transposeMatrix(int[][] array) {
+        // Implementation
+    }
+}
