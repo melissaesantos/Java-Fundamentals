0ca76419cebdfddc25941b3b1c04acc5876e2f31
Melissa Santos <melissasantos2202@gmail.com>
2024-07-25 15:27:52 -0600

    reordering and starting the basics

---
+++ arrayBasics.java
@@ -0,0 +1,7 @@
+public class arrayBasics{
+// here we will just be rviewing the basics of Java Arrays
+
+//ARRAY BASICS ***************************
+//1.1 Declare and initialize an array of intergers with 10 elements.
+    int[] arr = new int[10]
+}
--- arrays.java
+++ arrays.java
@@ -1,4 +1,4 @@
-package Arrays;
+
 public class arrays {
     //array basics:
 
