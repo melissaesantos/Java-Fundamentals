87fa7acd70d10148f53196e6b9866d44dbe367d4
Melissa Santos <melissasantos2202@gmail.com>
2024-08-02 15:56:46 -0600

    finished add song py

--- LinkedList Practice Python/LinkedList.py
+++ LinkedList Practice Python/LinkedList.py
@@ -3,5 +3,18 @@ from node import Node
 class LinkedList:
     def __init__(self):
         # Initialize the linked list
-        
-    # Other methods...
+        self.head = None
+        self.tail = None
+#add sonf at the end ofthe list 
+    def add_song(self,songName,artistName):
+        newNode = Node("HOTTOGO", "ChapelleRoan")
+        #lets check if this is the first node in the list
+        if not self.head:
+            self.head = newNode
+            self.tail =newNode
+        else:
+            #updating what comes next fter the tail
+            self.tail.next = newNode
+            #setting the tail to be the new node
+            self.tail = newNode
+
