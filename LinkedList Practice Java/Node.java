5a8180a73b739e10b21256dcf5e62fc37054f2b7
Melissa Santos <melissasantos2202@gmail.com>
2024-08-02 15:05:22 -0600

    finished node class

--- LinkedList Practice Java/Node.java
+++ LinkedList Practice Java/Node.java
@@ -2,6 +2,19 @@ public class Node {
     String songName;
     String artistName;
     Node next;
-    
-    // Constructor and methods...
+    //node has a memory address for where it is located in the computer
+    //a singly linked list has a head ->Node(value).next-> Node2(value).bext->
+    //accessing or looking for a node is O(n)
+    //adding or subrtracting a node is O(1)
+    //DOublly linked list also has a prev 
+   public Node(String songName, String artistName, Node next){
+        this.songName = songName;
+        this.artistName= artistName;
+        this.next = null; //intially the next is null 
+   }
+
+   @Override
+   public String toString(){
+    return songName + "by" + artistName;
+   }
 }
